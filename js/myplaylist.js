5064041680905f62d60cbf5f8b167c84e4a4726b
subin <subjsam@gmail.com>
2016-05-14 18:09:38 -0400

    Initial Commit

---
+++ js/myplaylist.js
@@ -0,0 +1,19 @@
+/**
+ * Created by 23rd and Walnut
+ * www.23andwalnut.com
+ * User: Saleem El-Amin
+ * Date: 6/8/11
+ * Time: 9:39 AM
+ */
+
+var myPlaylist = [
+
+    {
+        mp3:'mix/The Weeknd - Can\'t Feel My Face.mp3',
+        oga:'mix/1.ogg',
+        title:'Can\'t Feel My Face',
+        artist:'The Weeknd',
+        rating:5,
+        cover:'mix/bbtm.jpg'
+    }
+];
